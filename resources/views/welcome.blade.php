f60db8c84ebf91931d3f859c6193bfda4c7197ca
belalhamdan25 <belal.hamdan25@gmail.com>
2020-09-29 01:45:48 +0300

    test heroku

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel updated
+                    Laravel updated 2
                 </div>
 
                 <div class="links">
