a468f4dbda407592f11f03fb7c4d897feee6f29b
belalhamdan25 <belal.hamdan25@gmail.com>
2020-10-05 07:12:26 +0300

    fix seeding

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -21,13 +21,14 @@ class DatabaseSeeder extends Seeder
 
         $statuses = ['name' => 'Photoshop','Illustrator', 'Graphic design','Logo design','Microsoft word','Microsoft excel','Translation','HTML 5','CSS 3','PHP','Online marketing','Web development','After effect','Android','Javascript','Bootstrap','Vuejs','Reactjs','Jquery','Data Analysis','Website Design','Mobile App Development','Writing','Editing','Video Editing','Search Engine Optimization','Social Media Marketing','MYSQL','3D Design','Laravel','ASP','Microsoft .NET','Node js','Git','Swift','Wordpress','UX design','UI design','Responsive design','User modeling','Independent Sales','Training','Consulting','Voice-Over Acting','Career Coaching','Research','TypeScript','Technical recruiter','Education','Advertising','Electronic' ,'design','E-books','Landing pages','Sketch','Microsoft office','Adobe','Interior design','Ruby on rails'];
 
-         for ($i = 0; $i <= 57; $i++) {
+         for ($i = 0; $i < count($statuses)-1; $i++) {
             Tag::create(array(
                 'name' => $statuses[$i],
             ));
             }
 
-                for($i=0;$i<10;$i++){
+                //$i must be equal to Portfolio::class seed
+                for($i=0;$i<150;$i++){
                     DB::table('portfolio_tag')->insert([
                         'tag_id' => App\Tag::all()->random()->id,
                         'portfolio_id' => App\Portfolio::all()->random()->id,
