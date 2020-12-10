497f5ba788a84e02f5b2f1ae9e87875c2eeffc48
belalhamdan25 <belal.hamdan25@gmail.com>
2020-12-10 22:42:45 +0200

    fix password

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -108,7 +108,6 @@ class AuthController extends Controller
         $user->phone_number = request('phone_number');
         $user->location = request('location');
         $user->gender = request('gender');
-        $user->password  = Hash::make(request('password'));
 
         $user->save();
 
