47a20503b89223f42ea7ed0f403baaedc6d9b8b9
belalhamdan25 <belal.hamdan25@gmail.com>
2020-12-18 10:59:34 +0200

    portfolio create

--- app/Http/Controllers/PortfolioController.php
+++ app/Http/Controllers/PortfolioController.php
@@ -9,25 +9,25 @@ use App\Http\Resources\Portfolio\PortfolioCollection;
 use App\Http\Resources\Portfolio\PortfolioResource;
 use App\Tag;
 use App\Category;
+use Auth;
+use Illuminate\Support\Facades\Storage;
+
 class PortfolioController extends Controller
 {
     public function all()
     {
-        $portfolio = Portfolio::with('user','portfolioImages')->orderBy('id', 'desc')->paginate(18);
+        $portfolio = Portfolio::with('user', 'portfolioImages')->orderBy('id', 'desc')->paginate(18);
         return $portfolio;
-
     }
 
     public function portfolioShow(Portfolio $id)
     {
         return new PortfolioResource($id);
-
     }
 
     public function portfolioShowSkills(Portfolio $id)
     {
         return $id->tags;
-
     }
 
     public function search(Request $request)
@@ -37,26 +37,24 @@ class PortfolioController extends Controller
 
         if ($request->has('q')) {
 
-        $Portfolio = Portfolio::where('title', 'like', '%' . $request->get('q') . '%')->orWhere('desc', 'like', '%' . $request->get('q') . '%')
-        ->with('user','portfolioImages')->get();
+            $Portfolio = Portfolio::where('title', 'like', '%' . $request->get('q') . '%')->orWhere('desc', 'like', '%' . $request->get('q') . '%')
+                ->with('user', 'portfolioImages')->get();
 
-        if($Portfolio->count() > 0){
-            return $Portfolio;
-        }else{
+            if ($Portfolio->count() > 0) {
+                return $Portfolio;
+            } else {
+                return $error;
+            }
+        } else {
             return $error;
         }
-
-         }else{
-            return $error;
-
-         }
     }
 
-    public function portfolioShowImages(Request $request){
-        $portfolioId=$request->get('pm');
-        $images=Portfolio::find($portfolioId);
+    public function portfolioShowImages(Request $request)
+    {
+        $portfolioId = $request->get('pm');
+        $images = Portfolio::find($portfolioId);
         return $images->portfolioImages;
-
     }
 
     public function categoriesFilter(Request $request)
@@ -66,8 +64,8 @@ class PortfolioController extends Controller
         $results = [];
         for ($i = 0; $i < count($categoryId); $i++) {
             $tagfind = Category::find($categoryId[$i]);
-            $portfolioResults = $tagfind->portfolios()->with('user','portfolioImages')->orderBy('id', 'desc')
-            ->get();
+            $portfolioResults = $tagfind->portfolios()->with('user', 'portfolioImages')->orderBy('id', 'desc')
+                ->get();
             $results = array_merge($results, $portfolioResults->toArray());
         }
         return (array)$results;
@@ -83,7 +81,7 @@ class PortfolioController extends Controller
         $results = [];
         for ($i = 0; $i < count($tagid); $i++) {
             $tagfind = Tag::find($tagid[$i]);
-            $portfolioResults = $tagfind->portfolios()->with('user','portfolioImages')->orderBy('id', 'desc')->get();
+            $portfolioResults = $tagfind->portfolios()->with('user', 'portfolioImages')->orderBy('id', 'desc')->get();
             $results = array_merge($results, $portfolioResults->toArray());
         }
         return (array)$results;
@@ -92,13 +90,44 @@ class PortfolioController extends Controller
     public function skillFilter(Request $request)
     {
         $tagid = Tag::where('name', $request->get('skq'))->pluck('id')->first();
-        $tagFind=Tag::find($tagid);
-        return $tagFind->portfolios()->with('user','portfolioImages')->orderBy('id', 'desc')->get();
+        $tagFind = Tag::find($tagid);
+        return $tagFind->portfolios()->with('user', 'portfolioImages')->orderBy('id', 'desc')->get();
     }
 
-    public function portfoliosCreate(){
-        return "portfoliosCreate";
+    public function portfoliosCreate(Request $request)
+    {
+            $user = Auth::user();
+
+           $portfolio = New Portfolio;
+           $portfolio->user_id=$user->id;
+           $portfolio->title=$request->get('title');
+           $portfolio->desc=$request->get('desc');
+           $portfolio->link=$request->get('link');
+           $portfolio->date=$request->get('date');
+           $portfolio->category_id=$request->get('category_id');
+           $portfolio->save();
+
+           $portfolioTag = Portfolio::find($portfolio->id);
+           $portfolioTag->tags()->sync(request('tags_id'));
+           $portfolioTag->save();
+
+        $images_urls=[];
+                for($i=0;$i<count($request->portfolioImages_name);$i++){
+                    $portfolioImage_name = New PortfolioImage;
+                    // $imageName = $request->file('portfolioImages_name')->store('portfolio_images', 's3');
+                    $imageName = $request->portfolioImages_name[$i]->store('portfolio_images','s3');
+                    $portfolioImage_name->portfolio_id=$portfolio->id;
+                    $portfolioImage_name->name=Storage::disk('s3')->url($imageName);
+                    $portfolioImage_url=Storage::disk('s3')->url($imageName);
+                    Storage::disk('s3')->setVisibility($imageName,'public');
+                    $portfolioImage_name->save();
+                    array_push($images_urls,$portfolioImage_url);
+        }
+
+      return response()->json([
+            'portfolio'=>$portfolio,
+            'portfolioImages_name' => $images_urls,
+        ]);
+
     }
-
-
 }
