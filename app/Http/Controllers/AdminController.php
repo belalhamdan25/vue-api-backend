a4036fb532332436ed57a11017d3c75f83d5de45
belalhamdan25 <belal.hamdan25@gmail.com>
2020-12-21 21:33:25 +0200

    remove admin

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -9,12 +9,5 @@ use App\ProjectOffer;
 
 class AdminController extends Controller
 {
-    public function index(){
 
-        $usersCount=User::all()->count();
-        $projectsCount=Project::all()->count();
-        $offersCount=ProjectOffer::all()->count();
-
-        return view('index',compact('usersCount','projectsCount','offersCount'));
-    }
 }
