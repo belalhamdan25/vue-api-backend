d450cb357213fb89f5d0896505d7eca55c6742c1
belalhamdan25 <belal.hamdan25@gmail.com>
2020-12-25 00:03:39 +0200

    purshase

--- database/factories/PurchaseFactory.php
+++ database/factories/PurchaseFactory.php
@@ -10,8 +10,14 @@ $factory->define(Purchase::class, function (Faker $faker) {
         'project_id' => function(){
             return App\Project::all()->random()->id;
         },
+        'worker_id' => function(){
+            return App\User::all()->random()->id;
+        },
+        'status' => $faker->randomElement(['closed','in proccess','finished']),
         'user_id' => function(){
             return App\User::all()->random()->id;
         },
     ];
 });
+
+
--- database/migrations/2020_12_24_213747_create_purchases_table.php
+++ database/migrations/2020_12_24_213747_create_purchases_table.php
@@ -16,7 +16,9 @@ class CreatePurchasesTable extends Migration
         Schema::create('purchases', function (Blueprint $table) {
             $table->id();
             $table->integer('user_id');
+            $table->integer('worker_id');
             $table->integer('project_id');
+            $table->string('status');
             $table->timestamps();
         });
     }
