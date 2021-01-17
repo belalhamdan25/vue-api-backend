66366ef91bfd75ef7eeb727825ce7055ec390337
belalhamdan25 <belal.hamdan25@gmail.com>
2021-01-17 04:25:14 +0200

    project

--- database/factories/RateFactory.php
+++ database/factories/RateFactory.php
@@ -2,7 +2,7 @@
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
-use App\Model;
+use App\Rate;
 use Faker\Generator as Faker;
 
 $factory->define(Rate::class, function (Faker $faker) {
